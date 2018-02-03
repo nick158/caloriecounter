1c5017b48e97140d31001179e1de757e45eb0671
Nicholas Chan <nicholasdlchan60@gmail.com>
2018-02-03 16:38:22 -0500

    aws s3 configuration for java

--- s3.js
+++ s3.js
@@ -3,7 +3,7 @@ const aws = require('aws-sdk');
 aws.config.update({
   accessKeyId:"AKIAIPS5WU5KQZXC5T6Q" ,
   secretAccessKey: "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",
-  region: 'us-east-2'
+  region: 'us-east-1'
 });
 
 module.exports = new aws.S3({
